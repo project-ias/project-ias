0b2a892ecf69b3d33e9aa7f0a66ccb89a86b0c6d
rakaar <rka87338@gmail.com>
2021-06-26 05:10:50 +0530

    Frontend fetches both pyqs and content from backend

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -5,17 +5,19 @@ import { BACKEND_URL } from '../constants/constants'
 export default function SearchPage() {
 
     const [pyqs, setPyqs] = useState([])
+    const [content, setContent] = useState([])
 
     function handleChange(e) {
         console.log("e.",e.target.value)
         const query = e.target.value
         const data = { 'query': query }
-        const URL = `${BACKEND_URL}/search`
+        const PYQ_URL = `${BACKEND_URL}/search_pyq`
+        const Content_URL = `${BACKEND_URL}/search_content`
 
         if(query !== '') {
             console.log("Non empty query",query)
             axios
-            .post(URL, data)
+            .post(PYQ_URL, data)
             .then(res => {
                 console.log("res",res.data)
                 setPyqs(res.data.hits)
@@ -23,8 +25,20 @@ export default function SearchPage() {
             .catch(err => {
                 console.log("err is ",err)
             })
+
+            axios
+            .post(Content_URL, data)
+            .then(res => {
+                console.log("content res",res.data)
+                setContent(res.data.hits)
+            })
+            .catch(err => {
+                console.log("err is ",err)
+            })
+
         } else if(query === '') {
             setPyqs([])
+            setContent([])
         }
     }
 
@@ -43,15 +57,34 @@ export default function SearchPage() {
         <h1>Project IAS</h1>
 
         <input  onKeyUp={processChange}/>
-        {pyqs &&
-        pyqs.map(item => (
-            <div key={item['id']}>
-                <h4>{item['question']} ({item['year']})</h4>
-                <p><strong>Topics:</strong> {item['topics'].join(',')}</p>
-                <br/>
-            </div>
-        ))
-        
-        }
+        <table>
+            <tbody>
+                <tr>
+                    <td> 
+                        {pyqs &&
+                            pyqs.map(item => (
+                                <div key={item['id']}>
+                                    <h4>{item['question']} ({item['year']})</h4>
+                                    <p><strong>Topics:</strong> {item['topics'].join(',')}</p>
+                                    <br/>
+                                </div>
+                            ))
+                        }
+                    </td>
+                    <td>
+                        {content &&
+                            content.map(item => (
+                                <div key={item['id']}>
+                                    <h4>{item['link']} ({item['exam']})</h4>
+                                    <p><strong>Topics:</strong> {item['tags'].join(',')}</p>
+                                    <br/>
+                                </div>
+                            ))
+                            }
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+       
     </div>)
 }
