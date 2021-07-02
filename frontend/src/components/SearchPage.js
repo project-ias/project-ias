1d859a090500e1e032edf0564e9d5cc847ea8f3b
Sahil Shubham <sahil.shubham2000@gmail.com>
2021-07-02 20:17:23 +0530

    ran prettier and added classes;

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -1,103 +1,106 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import ReactHtmlParser from 'react-html-parser'
-import { parse } from 'node-html-parser'
-import { BACKEND_URL } from '../constants/constants'
-import { InstantSearch, SearchBox, Hits, Highlight, RefinementList } from 'react-instantsearch-dom';
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+import React, { useState } from "react";
+import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+import { parse } from "node-html-parser";
+import { BACKEND_URL } from "../constants/constants";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+  RefinementList,
+} from "react-instantsearch-dom";
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
 
-
-const searchClient = instantMeiliSearch(
-    "http://localhost:7700",
-    "masterKey"
-  );
+const searchClient = instantMeiliSearch("http://localhost:7700", "masterKey");
 
 function Hit(props) {
-    console.log("props is ",props)
-    return(
-        <div>
-            {props.hit.question} 
-            
-            <br/>
-            <b>Options:</b>
-            {props.hit.options.map(item => {
-                return(
-                    <div>
-                       <input type="checkbox"  value={item} name={item} />
-                        <label for={item}>{item}</label><br/>
-                    </div>
-                )
-            })}
-            
-            <br/>
-            <b>Explanation:</b>
-            {ReactHtmlParser(props.hit.explanation)}
-        </div>
-    )
-// return <Highlight attribute="name" hit={props.hit} />;
+  console.log("props is ", props);
+  return (
+    <div>
+      {props.hit.question}
+
+      <br />
+      <b>Options:</b>
+      {props.hit.options.map((item) => {
+        return (
+          <div>
+            <input type="checkbox" value={item} name={item} />
+            <label for={item}>{item}</label>
+            <br />
+          </div>
+        );
+      })}
+
+      <br />
+      <b>Explanation:</b>
+      {ReactHtmlParser(props.hit.explanation)}
+    </div>
+  );
+  // return <Highlight attribute="name" hit={props.hit} />;
 }
 
-export default function SearchPage () {
+export default function SearchPage() {
+  const [pyqs, setPyqs] = useState([]);
+  const [content, setContent] = useState([]);
 
-    const [pyqs, setPyqs] = useState([])
-    const [content, setContent] = useState([])
+  function removePrevNext(htmlString) {
+    let parsedHtml = parse(htmlString);
+    parsedHtml.querySelector(".next-post").remove();
+    return parsedHtml.toString();
+  }
 
-    function removePrevNext(htmlString) {
-        let parsedHtml = parse(htmlString)
-        parsedHtml.querySelector(".next-post").remove()
-        return parsedHtml.toString()
+  function handleChange(e) {
+    console.log("e.", e.target.value);
+    const query = e.target.value;
+    const data = { query: query };
+    const PYQ_URL = `${BACKEND_URL}/search_pyq`;
+    const Content_URL = `${BACKEND_URL}/search_content`;
+
+    if (query !== "") {
+      console.log("Non empty query", query);
+      axios
+        .post(PYQ_URL, data)
+        .then((res) => {
+          console.log("res", res.data);
+          setPyqs(res.data.hits);
+        })
+        .catch((err) => {
+          console.log("err is ", err);
+        });
+
+      axios
+        .post(Content_URL, data)
+        .then((res) => {
+          console.log("content res", res.data);
+          setContent(res.data.hits);
+        })
+        .catch((err) => {
+          console.log("err is ", err);
+        });
+    } else if (query === "") {
+      setPyqs([]);
+      setContent([]);
     }
-    
-    function handleChange(e) {
-        console.log("e.",e.target.value)
-        const query = e.target.value
-        const data = { 'query': query }
-        const PYQ_URL = `${BACKEND_URL}/search_pyq`
-        const Content_URL = `${BACKEND_URL}/search_content`
+  }
 
-        if(query !== '') {
-            console.log("Non empty query",query)
-            axios
-            .post(PYQ_URL, data)
-            .then(res => {
-                console.log("res",res.data)
-                setPyqs(res.data.hits)
-            })
-            .catch(err => {
-                console.log("err is ",err)
-            })
+  function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
 
-            axios
-            .post(Content_URL, data)
-            .then(res => {
-                console.log("content res",res.data)
-                setContent(res.data.hits)
-            })
-            .catch(err => {
-                console.log("err is ",err)
-            })
+  const processChange = debounce((e) => handleChange(e));
 
-        } else if(query === '') {
-            setPyqs([])
-            setContent([])
-        }
-    }
+  return (
+    <div className="main">
+      <h1 className="title">Project IAS</h1>
 
-    function debounce(func, timeout = 300){
-        let timer;
-        return (...args) => {
-          clearTimeout(timer);
-          timer = setTimeout(() => { func.apply(this, args); }, timeout);
-        };
-      }
-      
-    
-    const processChange = debounce((e) => handleChange(e));
-
-    return(<div>
-        <h1>Project IAS</h1>
-
-        {/* <input  onKeyUp={processChange}/>
+      {/* <input  onKeyUp={processChange}/>
                 <h1>PYQ Mains</h1>
                         {pyqs &&
                             pyqs.map(item => (
@@ -123,26 +126,22 @@ export default function SearchPage () {
 
                         } */}
 
-                 <h1>Prelims</h1>
-                    <InstantSearch
-                        indexName="prelims"
-                        searchClient={searchClient}
-                    >
-                        <SearchBox />
-                        {/* <RefinementList attribute="exam" /> */}
-                        <Hits hitComponent={Hit} />
-                    </InstantSearch>
-                  
-       
-    </div>)
-    // return(
-    //     <InstantSearch
-    //     indexName="pyqs"
-    //     searchClient={searchClient}
-    //   >
-    //     <SearchBox />
-    //     <RefinementList attribute="exam" />
-    //     <Hits hitComponent={Hit} />
-    //   </InstantSearch>
-    // )
+      <h2>Prelims</h2>
+      <InstantSearch indexName="prelims" searchClient={searchClient}>
+        <SearchBox />
+        {/* <RefinementList attribute="exam" /> */}
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
+    </div>
+  );
+  // return(
+  //     <InstantSearch
+  //     indexName="pyqs"
+  //     searchClient={searchClient}
+  //   >
+  //     <SearchBox />
+  //     <RefinementList attribute="exam" />
+  //     <Hits hitComponent={Hit} />
+  //   </InstantSearch>
+  // )
 }
