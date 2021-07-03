b1103a56db559affcd6ccf2b690b171d1d86660a
rakaar <rka87338@gmail.com>
2021-07-03 15:12:50 +0530

    Added code to split topics in pyqs

--- backend/add_pyq_to_ms.js
+++ backend/add_pyq_to_ms.js
@@ -14,9 +14,25 @@ fs.readFile('pyq_scrapers/pyqs.json', async (err, data) => {
     for(let i = 0; i < array_length; i++) {
         const id = crypto.randomBytes(20).toString('hex');
         json_data[i]["id"] = id
-        // console.log(json_data[i])
+        
+        // seperate topics
+        let seperated_topics = [];
+        for (let topic of json_data[i]['topics']) {
+            if(topic.includes(";")) { // eg - "Historical underpinnings & evolution; Features, amendments, significant provisions, basic structure; Comparison of Indian constitutional scheme with other countries’"
+                let split_arr = topic.split(";")
+                seperated_topics = seperated_topics.concat(split_arr)
+            } else if(topic.includes(",")) { // eg -"Salient aspects of Art, Architecture, literature from Ancient to Modern Times"
+                let split_arr = topic.split(',')
+                seperated_topics = seperated_topics.concat(split_arr)
+            } else {
+                seperated_topics.push(topic)
+            }
+        }
+
+        json_data[i]['topics'] = seperated_topics
         const added = await client.index('pyqs').addDocuments([ json_data[i] ])
         console.log("added ",added)
+        seperated_topics = []
     } 
 
 });
