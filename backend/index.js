5e48ad3ed835cbb19ad6f1dab76b6a9ba9fbdfb6
Raghavendra Kaushik <44907757+rakaar@users.noreply.github.com>
2021-07-05 16:25:50 +0530

    Remove unused axios

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const axios = require("axios");
 const { MeiliSearch } = require("meilisearch")
 
 const app = express();
