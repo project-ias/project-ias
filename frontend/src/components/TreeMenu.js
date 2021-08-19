3c5bc5d72f2d29925b662553af990cd06e580c63
rakaar <rka87338@gmail.com>
2021-08-20 00:28:40 +0530

    userMains in TreeMenu.js is now Empty array instead of empty string, if not stored locally

--- frontend/src/components/TreeMenu.js
+++ frontend/src/components/TreeMenu.js
@@ -31,98 +31,98 @@ const TreeNode = ({ node }) => {
     node.category === "examSubType" || node.category === "mainTopic";
   var percentSolved = 0,
     totalQuestions = 0;
-    var pathColor = "red";
+  var pathColor = "red";
 
-    if (label === null || label === undefined) label = node;
+  if (label === null || label === undefined) label = node;
 
-    if (showPercentage) {
-      var userMains = localStorage.getItem("userMains") || "";
-      userMains = JSON.parse(userMains).map((item) => item.questionID);
-      
-      var temp = 0;
-      for (var i = 0; i < userMains.length; i++) {
-        if (node.category === "mainTopic") {
-          if (node.questions.includes(userMains[i])) temp++;
-          totalQuestions = node.questions.length;
-        } else {
-          for (var j = 0; j < node.children.length; j++) {
-            if (node.children[j].questions.includes(userMains[i])) temp++;
-            totalQuestions += node.children[j].questions.length;
-          }
+  if (showPercentage) {
+    var userMains = localStorage.getItem("userMains") || "[]";
+    userMains = JSON.parse(userMains).map((item) => item.questionID);
+
+    var temp = 0;
+    for (var i = 0; i < userMains.length; i++) {
+      if (node.category === "mainTopic") {
+        if (node.questions.includes(userMains[i])) temp++;
+        totalQuestions = node.questions.length;
+      } else {
+        for (var j = 0; j < node.children.length; j++) {
+          if (node.children[j].questions.includes(userMains[i])) temp++;
+          totalQuestions += node.children[j].questions.length;
         }
       }
-      if (node.category === "examSubType") totalQuestions /= userMains.length; //in case of examSubType, the questions get added multiple times as it is put inside the userMains for loop.
-      percentSolved = Math.floor((temp * 100) / totalQuestions);
-      if (percentSolved < 30) pathColor = "red";
-      else pathColor = "green";
     }
+    if (node.category === "examSubType") totalQuestions /= userMains.length; //in case of examSubType, the questions get added multiple times as it is put inside the userMains for loop.
+    percentSolved = Math.floor((temp * 100) / totalQuestions);
+    if (percentSolved < 30) pathColor = "red";
+    else pathColor = "green";
+  }
 
-    const topicSelectHandler = (query) => {
-      if (node.category === "examType") return;
-      history.push(`/?${query}`);
-      history.go(`/?${query}`);
-    };
+  const topicSelectHandler = (query) => {
+    if (node.category === "examType") return;
+    history.push(`/?${query}`);
+    history.go(`/?${query}`);
+  };
 
-    return (
-      <li className="tree-menu-list">
-        <div className="tree-menu-item">
-          {hasChild && (
-            <div
-              className={`tree-menu-caret ${
-                childVisible ? "tree-menu-active" : ""
-              }`}
-              onClick={() => setChildVisible((value) => !value)}
-            >
-              <FontAwesomeIcon icon={faCaretSquareRight} />
-            </div>
-          )}
-
-          {!hasChild && (
-            <div>
-              <FontAwesomeIcon icon={faCircle} />
-            </div>
-          )}
-
+  return (
+    <li className="tree-menu-list">
+      <div className="tree-menu-item">
+        {hasChild && (
           <div
-            className="tree-menu-label"
-            onClick={() => topicSelectHandler(label)}
+            className={`tree-menu-caret ${
+              childVisible ? "tree-menu-active" : ""
+            }`}
+            onClick={() => setChildVisible((value) => !value)}
           >
-            {label}
-            <span className="tree-menu-solved-fraction">
-              {showPercentage ? ` ( ${temp} / ${totalQuestions} ) ` : ""}
-            </span>
-          </div>
-          {showPercentage && localStorage.getItem("userEmail") && (
-            <div className="tree-menu-solved">
-              <CircularProgressbar
-                className="tree-menu-solved-svg"
-                value={percentSolved}
-                text={`${percentSolved}%`}
-                background
-                backgroundPadding={0}
-                styles={buildStyles({
-                  // backgroundColor: "#333",
-                  // textColor: "#fefaee",
-                  backgroundColor: "transparent",
-                  textColor: "#333",
-                  textSize: "1.4rem",
-                  pathColor: pathColor,
-                  trailColor: "#000",
-                })}
-              />
-            </div>
-          )}
-        </div>
-
-        {hasChild && childVisible && (
-          <div>
-            <ul className="tree-menu-column">
-              <TreeMenu data={node.children} />
-            </ul>
+            <FontAwesomeIcon icon={faCaretSquareRight} />
           </div>
         )}
-      </li>
-    );
+
+        {!hasChild && (
+          <div>
+            <FontAwesomeIcon icon={faCircle} />
+          </div>
+        )}
+
+        <div
+          className="tree-menu-label"
+          onClick={() => topicSelectHandler(label)}
+        >
+          {label}
+          <span className="tree-menu-solved-fraction">
+            {showPercentage ? ` ( ${temp} / ${totalQuestions} ) ` : ""}
+          </span>
+        </div>
+        {showPercentage && localStorage.getItem("userEmail") && (
+          <div className="tree-menu-solved">
+            <CircularProgressbar
+              className="tree-menu-solved-svg"
+              value={percentSolved}
+              text={`${percentSolved}%`}
+              background
+              backgroundPadding={0}
+              styles={buildStyles({
+                // backgroundColor: "#333",
+                // textColor: "#fefaee",
+                backgroundColor: "transparent",
+                textColor: "#333",
+                textSize: "1.4rem",
+                pathColor: pathColor,
+                trailColor: "#000",
+              })}
+            />
+          </div>
+        )}
+      </div>
+
+      {hasChild && childVisible && (
+        <div>
+          <ul className="tree-menu-column">
+            <TreeMenu data={node.children} />
+          </ul>
+        </div>
+      )}
+    </li>
+  );
 };
 
 export default TreeMenu;
