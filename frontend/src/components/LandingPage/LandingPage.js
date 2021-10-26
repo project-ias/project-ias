d06d8a16c69c03f8e6879fe7f3e3ae776333bdcc
chirag828049 <cghosh828049@gmail.com>
2021-10-26 22:04:20 +0530

    Added start box

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -3,6 +3,7 @@ import DemoVideo from "./DemoVideo";
 import HeaderCard from "./HeaderCard";
 import HeaderTitle from "./HeaderTitle";
 import NavBar from "./NavBar";
+import StartBox from "./StartBox";
 
 const LandingPage = () => {
     return (
@@ -16,6 +17,9 @@ const LandingPage = () => {
             </div>
             <div className="demo-video-div">
                 <DemoVideo/>
+            </div>
+            <div className="start-box-div">
+                <StartBox/>
             </div>
         </div>
     )
---
+++ frontend/src/components/LandingPage/StartBox.js
@@ -0,0 +1,13 @@
+const StartBox = () => {
+
+    return (
+        <div className="start-box">
+            <div className="start-title">So where do we start ?</div>
+            <div className="start-btn">
+                <button className="current-user-auth-btn black-btn">RIGHT HERE</button>
+            </div>
+        </div>
+    )
+}
+
+export default StartBox;
