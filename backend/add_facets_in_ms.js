1eaee838d07fdb27073bdf1efd807d2bd87cde99
chirag828049 <cghosh828049@gmail.com>
2021-11-02 20:00:02 +0530

    Deleted obsolete files

--- backend/add_facets_in_ms.js
+++
@@ -1,9 +0,0 @@
-const { MeiliSearch } = require("meilisearch");
-const keys = require("./config/keys");
-
-const client = new MeiliSearch({
-  host: keys.MEILISEARCH_URL,
-  apiKey: "masterKey",
-});
-
-client.index("pyqs").updateAttributesForFaceting(["exam", "topics"]);
