fcbc08d56b4ec5d76428fb78790541d0c2024541
chirag828049 <cghosh828049@gmail.com>
2022-02-07 10:42:40 +0530

    Moved to netlify;

--- frontend/src/constants/envConstants.js
+++ frontend/src/constants/envConstants.js
@@ -2,7 +2,7 @@ var ENV_BACKEND_URL, ENV_FRONTEND_URL;
 
 if (process.env.NODE_ENV === "production") {
   ENV_BACKEND_URL = "https://api.project-ias.com";
-  ENV_FRONTEND_URL = "https://project-ias.com";
+  ENV_FRONTEND_URL = "https://www.project-ias.com";
 } else if (process.env.NODE_ENV === "development") {
   ENV_BACKEND_URL = "http://localhost:5000";
   ENV_FRONTEND_URL = "http://localhost:3000";
